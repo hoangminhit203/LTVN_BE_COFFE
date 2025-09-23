5f63a6f3c65fee5da828af48f29d195d4337d58b
truongminhtrii <hoangminhit203@gmail.com>
2025-09-23 21:54:17 +0700

    delete: WeatherForceCast

---
+++ LVTN_BE_COFFE/Domain/IServices/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using LVTN_BE_COFFE.Domain.VModel;
+using LVTN_BE_COFFE.Infrastructures.Entities;
+
+namespace LVTN_BE_COFFE.Domain.IServices
+{
+    public interface IProductService
+    {
+        Task<ProductResponse?> CreateProduct(ProductRequest request);
+        Task<ProductResponse?> UpdateProduct(ProductRequest request, int productId);
+        Task<bool?> DeleteProduct(int productId); // Delete nên trả bool thay vì Products
+        Task<List<ProductResponse?>> GetAllProducts();
+        Task<ProductResponse?> GetProduct(int productId);
+        Task<bool> FindByName(string name);
+
+    }
+}
---
+++ LVTN_BE_COFFE/Domain/VModel/ProductRequest.cs
@@ -0,0 +1,14 @@
+﻿using LVTN_BE_COFFE.Infrastructures.Entities;
+
+namespace LVTN_BE_COFFE.Domain.VModel
+{
+    public class ProductRequest
+    {
+        public int ProductTypeId { get; set; }
+        public string Sku { get; set; }//mã sản phẩm
+        public string Name { get; set; }
+        public decimal BasePrice { get; set; }// giá cơ bản
+        public bool IsActive { get; set; }
+        public ProductType ProductType { get; set; }
+    }
+}
