77baa7e8175a5b5d39592352304b7dc05e1ec239
truongminhtrii <hoangminhit203@gmail.com>
2025-09-27 08:15:10 +0700

    chore: fix dbcontext data base
    +Product

--- LVTN_BE_COFFE/Infrastructures/Entities/AppDbContext.cs
+++ LVTN_BE_COFFE/Infrastructures/Entities/AppDbContext.cs
@@ -7,5 +7,32 @@ namespace LVTN_BE_COFFE.Infrastructures.Entities
     public class AppDbContext : IdentityDbContext<IdentityUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<RefreshToken> RefreshTokens { get; set; } = null!;
+        // Bảng sản phẩm
+        public DbSet<Products> Products { get; set; }
+        // Bảng loại sản phẩm (trà sữa, topping, đồ ăn kèm…)
+        public DbSet<ProductType> ProductTypes { get; set; }
+
+        // Bảng chi nhánh (mỗi quán/chi nhánh)
+        public DbSet<Branch> Branches { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Quan hệ: ProductType 1 - nhiều Products
+            builder.Entity<ProductType>()
+                .HasMany(pt => pt.Products)
+                .WithOne(p => p.ProductType)
+                .HasForeignKey(p => p.ProductTypeId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Quan hệ: Branch 1 - nhiều Products
+            builder.Entity<Branch>()
+                .HasMany(b => b.Products)
+                .WithOne(p => p.Branch)
+                .HasForeignKey(p => p.BranchId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
