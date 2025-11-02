29bcd546725cad396e3b1da37b35f47d8e6794f7
toby bui <Toby@digityze.asia>
2025-11-02 13:09:21 +0700

    update

--- LVTN_BE_COFFE/Domain/IServices/IProductVariantService.cs
+++ LVTN_BE_COFFE/Domain/IServices/IProductVariantService.cs
@@ -1,13 +1,13 @@
-﻿using LVTN_BE_COFFE.Domain.VModel;
+﻿//using LVTN_BE_COFFE.Domain.VModel;
 
-namespace LVTN_BE_COFFE.Domain.IServices
-{
-    public interface IProductVariantService
-    {
-        Task<IEnumerable<ProductVariantResponse>> GetAllVariants();
-        Task<ProductVariantResponse?> GetVariantById(int id);
-        Task<ProductVariantResponse> CreateVariant(ProductVariantCreateVModel model);
-        Task<ProductVariantResponse?> UpdateVariant(ProductVariantUpdateVModel model);
-        Task<bool> DeleteVariant(int id);
-    }
-}
+//namespace LVTN_BE_COFFE.Domain.IServices
+//{
+//    public interface IProductVariantService
+//    {
+//        Task<IEnumerable<ProductVariantResponse>> GetAllVariants();
+//        Task<ProductVariantResponse?> GetVariantById(int id);
+//        Task<ProductVariantResponse> CreateVariant(ProductVariantCreateVModel model);
+//        Task<ProductVariantResponse?> UpdateVariant(ProductVariantUpdateVModel model);
+//        Task<bool> DeleteVariant(int id);
+//    }
+//}
